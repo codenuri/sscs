5587a7666948aa96208054f1aac69ac785e5029d
ecourse <smkang@codenuri.co.kr>
2024-06-24 14:06:08 +0900

    Add files via upload

--- DAY1/06_loop4_foreach.cs
+++ DAY1/06_loop4_foreach.cs
@@ -11,8 +11,15 @@ for( int i = 0; i < 10; i++)
 
 // 핵심 : foreach 구문
 foreach (int e in x) // x의 모든 요소를 차례대로 e 에 담기
-{
+{                    // x의 위치에는 열거 가능한(enumerable) 한 모든 객체가
+                    // 올수 있습니다.
     WriteLine($"{e}, ");
 }
 WriteLine();
 
+int k = 0;
+
+foreach (int e in k) // k 는 enumerable 하지 않습니다. error 메세지 읽어보세요
+{
+
+}
