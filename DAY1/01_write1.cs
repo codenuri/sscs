a184c9582cf03c6ecc0d6b6f09dfc52db71e5edf
ecourse <smkang@codenuri.co.kr>
2024-06-24 10:11:28 +0900

    Add files via upload

---
+++ DAY1/01_write1.cs
@@ -0,0 +1,20 @@
+// github.com/codenuri/sscs 에서 DAY1.zip 
+// DAY1 프로젝트 open 후 "CTRL+F5"로 확인해보세요
+
+
+// entry point : 프로그램의 진입점.  8page
+
+// 방식 #1. 약속된 함수(main)에서 시작하는 언어
+// => C/C++, Java등
+// => C# 9.0 이전에는 이방식만 가능
+
+// 방식 #2. 소스파일의 1번째 줄 부터 실행되는 언어
+// => Python, swift 
+// => C# 9.0 부터는 이방식도 지원
+//    ("top level programming" 이라고 합니다.)
+// => 간단한 테스트 코드에 적합
+
+// 즉, C# 의 위 2가지 방식을 모두 지원. 
+
+
+System.Console.WriteLine("Hello, C#");
