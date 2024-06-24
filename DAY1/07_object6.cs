0fbcbc80958ee6f7e1910be317dd484226e595e1
ecourse <smkang@codenuri.co.kr>
2024-06-24 15:17:17 +0900

    Add files via upload

---
+++ DAY1/07_object6.cs
@@ -0,0 +1,10 @@
+using static System.Console;
+
+string s = "abcd";
+
+// 메소드와 프라퍼티
+// 메소드 : 객체와 관련된 함수. C++의 멤버 함수
+// 프라퍼티 : 객체의 속성. C# 고유 문법, ()가 없음. 내일 자세히
+
+string s1 = s.ToString();   // () 가 있음. 메소드
+int n = s.Length;           // () 가 없음. Property
