65883fe79c809c1e6890dc4d96aaf563c9daa4f7
ecourse <smkang@codenuri.co.kr>
2024-06-25 08:46:29 +0900

    Add files via upload

---
+++ DAY2/02_mutabe_vs_immutable1.cs
@@ -0,0 +1,20 @@
+﻿// 30 page ~
+// mutable   : 객체의 속성을 변경할 수 있는 것 
+// immutable : 객체의 속성을 변경할 수 없는 것 
+
+// C# 에서
+// 배열은 "mutable" 입니다.
+// string "immutable" 입니다.
+
+int[] x = { 1, 2, 3 };
+x[0] = 10; // ok
+
+
+string s = "ABC";
+s[0] = 'X'; // error
+
+s = "XY"; // 왜 에러가 아닌가요 ?
+          // 기존 객체를 변경한 것이 아니라
+          // "새로운 객체를 만들고 s가 새로운 객체를 가리키는 것"
+          // s = new string("XY")
+
