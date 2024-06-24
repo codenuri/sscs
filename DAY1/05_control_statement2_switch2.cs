cde11794950b80554a0e2af33d22b79b7f00e024
ecourse <smkang@codenuri.co.kr>
2024-06-24 17:14:37 +0900

    Add files via upload

--- DAY1/05_control_statement2_switch2.cs
+++ DAY1/05_control_statement2_switch2.cs
@@ -10,11 +10,15 @@ switch (num)
         Console.WriteLine("1");
         break;
 
-    case 2:
-        Console.WriteLine("1");
-        break;
-    case 3:
-        Console.WriteLine("1");
+    case 2:  
+        Console.WriteLine("2");
+        // break;   // 의도적인 break 의 생략 - fallthrough 라고 합니다.
+                    // C/C++ 가능, C#/Rust 에러
+        // fallthrouth  // RUST 는 이렇게 적어야 합니다.
+        goto case 3;    // C#은 이렇게 하면 됩니다.
+
+    case 3:  
+        Console.WriteLine("3");    
         break;
 
 
