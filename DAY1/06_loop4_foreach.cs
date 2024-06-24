cc4882ca9a589b0e078fabf03e409c6c2c68db00
ecourse <smkang@codenuri.co.kr>
2024-06-24 14:11:29 +0900

    Add files via upload

--- DAY1/06_loop4_foreach.cs
+++ DAY1/06_loop4_foreach.cs
@@ -19,7 +19,21 @@ WriteLine();
 
 int k = 0;
 
-foreach (int e in k) // k 는 enumerable 하지 않습니다. error 메세지 읽어보세요
-{
+foreach (int e in k) // k 는 enumerable 하지 않습니다. 
+{                     // error 메세지 읽어보세요
 
 }
+
+// foreach 의 원리
+// => foreach(int e in obj) 일때
+// => var it = obj.GetEnumerator() 를 호출하게 됩니다.
+// => 따라서, 반드시 obj 에는 GetEnumerator() 가 있어야 합니다.
+
+// C#, Java, C++, Python 모두 동일한 원리
+
+// C# 제어문 정리
+
+// 2개의 조건문 : if, switch
+//               switch 는 expression 가능
+
+// 4개의 반복문 : while, do~while, for, foreach
