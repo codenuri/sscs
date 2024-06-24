f606b2f8ac3a54021917042f75263d9f2754e4a1
ecourse <smkang@codenuri.co.kr>
2024-06-24 15:39:02 +0900

    Add files via upload

---
+++ DAY1/value_reference.cpp
@@ -0,0 +1,15 @@
+﻿// value_reference.cpp
+
+// C++ 코드입니다. C++을 모르시는 분들도, 개념만 잘 알아두세요
+// => 교재 27page 참고
+struct Point
+{
+	int x, y;
+};
+
+int main()
+{
+	Point pt;	// stack 에 객체 생성
+			
+	Point* p1 = new Point;	// heap 에 객체 생성
+}
