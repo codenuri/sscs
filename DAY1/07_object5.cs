5808a0475c55b6a70d13e694cf4a68eeff7c072a
ecourse <smkang@codenuri.co.kr>
2024-06-24 15:14:58 +0900

    Add files via upload

---
+++ DAY1/07_object5.cs
@@ -0,0 +1,17 @@
+int    n = 0;
+double d = 0;
+string s = "A";
+
+
+// 객체지향 프로그램의 핵심
+// => 기반 클래스 타입으로 파생 클래스 객체를 가리킬수 있다.
+// => "가리 킨다?" 는 말은 다음시간에 자세히
+object o1 = n;
+object o2 = d;
+object o3 = s;
+
+// object 는 별명일 뿐입니다. 실제 타입은
+// System.Object 입니다.
+Object o5 = n;
+System.Object o6 = n;
+
