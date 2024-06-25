b5ec02f1decce92e0425acc5d08d6d95ea210f34
ecourse <smkang@codenuri.co.kr>
2024-06-25 16:56:32 +0900

    Add files via upload

---
+++ DAY2/07-1_ref_readonly3.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static void RefParameter(ref int x) { }
+    public static void InParameter(in int x)  {  }
+    public static void RefReadonlyParameter(ref readonly int x){}
+
+    public static void Main()
+    {
+        int n = 0;
+
+        RefParameter(ref n);        // ok
+        InParameter(in n);          // ok. in 생략 가능
+        RefReadonlyParameter(ref n);// ok
+
+    }
+}
+
