2b3c8469430f1eb336c471e37a4763fd34e1ccbb
ecourse <smkang@codenuri.co.kr>
2024-06-25 13:40:43 +0900

    Add files via upload

--- DAY2/05_method1_named_argument1.cs
+++ DAY2/05_method1_named_argument1.cs
@@ -14,5 +14,27 @@ class Program
 
         // 아래처럼 호출하면 어떨까요 ?
         SetRect(x:10, y:10, width:30, height:30);
+
+        // #1. 용어
+        SetRect(10, 10, 30, 30); // Positional Argument
+        SetRect(x: 10, y: 10, width: 30, height: 30); // Named Argument
+                                                      // C#, Objective-C, swift 등에서 사용
+                                                      // java, C++, Python 에 없는 문법
+
+        // #2. Named Argument 는 인자 순서 변경 가능합니다.
+        SetRect(width: 10, y: 10, height: 30, x: 30);
+
+        // #3. Positional 과 Named 를 섞어서 사용해도 됩니다.
+        SetRect(10, 10, width:30, height:30);
+        SetRect(x:10, y:10, 30, 30);
+
+        // #4. 주의 사항
+        // Named Argument 의 순서가 변경된 경우
+        // 이후 부터는 Positional 은 사용할수 없습니다.
+        SetRect(y: 10, x: 10, 30, 30); // error
+
+        SetRect(x: 10, 10, Width:30, 30); // ok. 순서 변경 안됨.
+
+
     }
 }
