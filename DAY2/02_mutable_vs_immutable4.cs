3dc896890a137e071325440fc031c6c03abca95d
ecourse <smkang@codenuri.co.kr>
2024-06-25 09:13:13 +0900

    Add files via upload

---
+++ DAY2/02_mutable_vs_immutable4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+// #1. string 은 immutable 합니다.
+string s = "AB";
+//s[0] = 'X'; // error.
+
+// string 의 메소드 중에서 "변경"하는 메소드는 없습니다.
+string s1 = s.ToLower(); // s 가 변경되는 것이 아니라!
+                         // s의 문자열 "AB" 가 소문자로된 "ab"
+                         // 를 가진 새로운 객체를 반환한것
+
+// mutable 한 string 이 필요 하면 StringBuilder 를 사용하세요
+StringBuilder sb = new StringBuilder("ABCD");
+
+sb[0] = 'X'; // ok
+
+WriteLine(sb);
