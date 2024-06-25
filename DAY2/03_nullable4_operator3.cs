dd6de5877fe8c1c7ea946121d080b037646328cd
ecourse <smkang@codenuri.co.kr>
2024-06-25 12:50:45 +0900

    Add files via upload

--- DAY2/03_nullable4_operator3.cs
+++ DAY2/03_nullable4_operator3.cs
@@ -11,8 +11,27 @@ object o1 = s1;
 string s2 = (string)o1; // ok
 
 
+// #3. 
+
+int n1 = o1;        // compile error
+int n1 = (int)o1; 	// o1이 가리키는 것은 int 아님. 
+                    // runtime error(예외 발생)
+
+// #3. is 연산자
+if ( o1 is int )
+{
+    int n1 = (int)o1;
+}
+
+if (o1 is int n2)
+{
+    // n2 사용
+}
+
+// #4. as 연산자 사용
+int  n3 = (int)o1;    // o1이 가리키는 곳인 int 아니면 예외
+int? n4 = o1 as int?; // o1이 가리키는 곳인 int 아니면 null반환
+int  n5 = o1 as int;  // error. 
 
 
-int n1 = o1;
-int n1 = (int)o1; 	
 
