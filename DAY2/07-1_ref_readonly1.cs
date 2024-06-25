16153cf091c186886e3ec2366aaec84cf366fd53
ecourse <smkang@codenuri.co.kr>
2024-06-25 16:32:49 +0900

    Add files via upload

---
+++ DAY2/07-1_ref_readonly1.cs
@@ -0,0 +1,18 @@
+﻿// 07-1_ref_readonly1.cs
+using static System.Console;
+
+class Program
+{
+    public static void InParameter(int x)
+    {
+        x = 10;
+    }
+    public static void Main()
+    {
+        int n = 0;
+
+        InParameter(n);
+
+        WriteLine(n);    
+    }
+}
