145b5f53069504876fde571b69341f42fe4ced05
ecourse <smkang@codenuri.co.kr>
2024-06-26 08:40:52 +0900

    Add files via upload

--- DAY3/01_class2.cs
+++ DAY3/01_class2.cs
@@ -16,11 +16,20 @@ class Rect
         return (left - right) * (bottom - top);
     }
 }
-
 class Program
 {
     public static void Main()
     {
+        // 위처럼 Rect 라는 타입을 라이브러리 설계자가 만들어 주면
+        // 일반 개발자는 아래 처럼 사용하면 됩니다.
+        Rect rc = new Rect();
+
+        rc.left = 10;
+        rc.top = 10;
+        rc.right = 30;
+        rc.bottom = 30;
+
+        int n = rc.GetArea();
 
     }
 }
