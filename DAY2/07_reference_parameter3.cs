546a82d64bdfb9e74d306794f2ee1d6e44f2fa38
ecourse <smkang@codenuri.co.kr>
2024-06-25 15:56:29 +0900

    Add files via upload

--- DAY2/07_reference_parameter3.cs
+++ DAY2/07_reference_parameter3.cs
@@ -10,22 +10,32 @@ class Point
 
 class Program
 {
+    // 규칙 3. reference 타입을 "ref" 로 받으면
+    // 1. 객체의 속성도 변경할수 있고
+    // 2. 
     public static void RefModifier(ref Point pt)
     {
         pt.x = 2;
         pt.y = 2;
 
-//        pt = new Point(3, 3);
+
+        // int n = 0;
+        // ref int x = ref n; 일때
+        // n = 10; // 10을 n 에 넣습니다.
+        // x = 10; // x는 n 을 가리키는 reference 이므로
+                    // 10은 x을 따라간곳 n 에 넣게 됩니다.
+
+        // pt 는 ref 입니다. 즉, main 의 p1 을 가리키는 reference
+        // 아래 코드는 결국
+        pt = new Point(3, 3); // pt가 가리키는 p1 = new Point(3,3)
     }
 
     public static void Main()
     {
         Point p1 = new Point(1, 1);
-        Point p2 = p1;
 
         RefModifier(ref p1);
 
         WriteLine($"{p1.x} {p1.y}");
-        WriteLine($"{p2.x} {p2.y}");
     }
 }
