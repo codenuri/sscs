13bb7079ab25fb2fd85b964b10c141d07467d164
ecourse <smkang@codenuri.co.kr>
2024-06-25 15:07:21 +0900

    Add files via upload

---
+++ DAY2/06_parameter_modifier4.cs
@@ -0,0 +1,21 @@
+using static System.Console;
+
+class Program
+{
+	public static void Swap( ???)
+	{
+		// 구현해 보세요
+	}
+
+	public static void Main()
+	{
+		int x = 1;
+		int y = 2;
+
+		// Swap 을 만들어 보세요
+		Swap( ? , ? );
+
+
+		WriteLine($"{x}, {y}"); // 2, 1 나와야 합니다.
+	}
+}
