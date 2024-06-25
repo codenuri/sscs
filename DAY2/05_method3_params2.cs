87a76a1654b119d170dadf4adbbc582b1941d666
ecourse <smkang@codenuri.co.kr>
2024-06-25 14:03:52 +0900

    Add files via upload

---
+++ DAY2/05_method3_params2.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    // 인자로 int 만 받을수 있습니다.
+    public static void M1(int n) { }
+
+    // object 는 대부분의 타입의 기반 클래스 입니다.
+    // => 모든 객체는 object 로 가리킬수 있습니다.
+    public static void M2(object n) { }
+
+    public static void Main()
+    {
+        M1(10);
+        M1("ABCD");
+
+        M2(10);
+        M2("ABCD");
+    }
+}
