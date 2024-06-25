ced29a72c4a8ce080ccf05ed3df0f6300d42da1d
ecourse <smkang@codenuri.co.kr>
2024-06-25 10:50:56 +0900

    Add files via upload

---
+++ DAY2/03_nullable1.cs
@@ -0,0 +1,24 @@
+﻿// 36 page ~
+
+// #1. reference type 은 null 이 될수 있습니다.
+// => 객체(값)없음을 나타낼수 있습니다.
+string s1 = "hello";
+string s2 = null;	// ok	
+
+// #2. value type 은 null 이 될수 없습니다.
+// => 값 없음을 표현할수 없습니다.
+int n1 = 0;
+// int n2 = null;	// error
+
+int some_work(int arg)
+{
+    // 반환 값으로 실패를 알리고 싶다
+    return 0;   // 0은 "값없음"을 나타내지는 않습니다.
+                // 단지, 약속으로 0, -1 등을 "실패(값없음)"으로 약속한것일뿐
+}
+ 
+
+  
+
+
+
