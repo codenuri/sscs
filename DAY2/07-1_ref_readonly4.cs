0a9d35c127d323f67c6b4568f08a975d65c8e057
ecourse <smkang@codenuri.co.kr>
2024-06-25 17:08:35 +0900

    Add files via upload

---
+++ DAY2/07-1_ref_readonly4.cs
@@ -0,0 +1,24 @@
+﻿// 58page 그림 ~ 
+using static System.Console;
+
+class Point
+{
+    public int x = 0;
+    public int y = 0;
+    public Point(int a, int b) => (x, y) = (a, b);
+}
+
+class Program
+{
+    public static void InParameter(in Point pt)
+    {
+        pt.x = 2;
+        pt.y = 2;
+    }
+    public static void Main()
+    {
+        Point p1 = new Point(1, 1);
+        InParameter(p1);
+
+    }
+}
