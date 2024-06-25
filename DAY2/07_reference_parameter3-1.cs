d5a9b888420a7149e8bca18882ea3e60a3f49a02
ecourse <smkang@codenuri.co.kr>
2024-06-25 16:06:47 +0900

    Add files via upload

--- DAY2/07_reference_parameter3-1.cs
+++ DAY2/07_reference_parameter3-1.cs
@@ -8,10 +8,13 @@ class Program
     {
         int n = 10;
 
+        // F(ref int x) / F(ref n)
         ref int x = ref n;
 
-        n = 20;
-        x = 30;
+        n = 20; // 자신의 메모리에 20 넣기
+        x = 30; // x는 reference 이므로
+                // 따라간 곳(대상체)에 넣게 됩니다.
+                // C언어(*x = 30)
 
         Console.WriteLine($"{x}");
 
