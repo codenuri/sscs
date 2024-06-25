82cb344e8ab1874742242b442a7090178e7f8721
ecourse <smkang@codenuri.co.kr>
2024-06-25 15:15:38 +0900

    Add files via upload

---
+++ DAY2/06_parameter_modifier5.cs
@@ -0,0 +1,18 @@
+using static System.Console;
+
+class Program
+{
+	public static void Main()
+	{
+//		string s = "10";
+        string s = "ABC";
+
+//      int n = int.Parse(s); // s 가 int로 변경 불가능하면
+							  // runtime error (예외) 발생. 
+
+		// C# 메소드의 특징
+		// "Try" 로 시작하는 메소드는 성공/실패 여부를 out parameter로
+		// 알려 줍니다.
+		int n = int.
+	}
+}
