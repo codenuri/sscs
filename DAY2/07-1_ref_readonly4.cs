b5b0eab754a53ff96a21fea6db52289d8f7b000b
ecourse <smkang@codenuri.co.kr>
2024-06-25 17:14:07 +0900

    Add files via upload

--- DAY2/07-1_ref_readonly4.cs
+++ DAY2/07-1_ref_readonly4.cs
@@ -10,10 +10,20 @@ class Point
 
 class Program
 {
+    // reference type 을 in 으로 전달할때
+    // in 으로 전달하면
+    // 1. 복사본이 아닌 주소 전달(reference 전달)
+    // 2. 가리키는 곳을 변경할수 없음.
+
+    // 따라서, 객체는 수정할수 있지만
+    // 새로운 객체를 만들어서 담아올수는 없다!!
+    // => 메모리를 잘생각해 보세요.
     public static void InParameter(in Point pt)
     {
-        pt.x = 2;
-        pt.y = 2;
+        pt.x = 2; // ok
+        pt.y = 2; // ok
+
+        pt = new Point(3, 3); // error
     }
     public static void Main()
     {
