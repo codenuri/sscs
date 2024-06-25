82191b86cc720055933c1e4bd633baaf788d9cd1
ecourse <smkang@codenuri.co.kr>
2024-06-25 16:16:56 +0900

    Add files via upload

--- DAY2/07_reference_parameter4.cs
+++ DAY2/07_reference_parameter4.cs
@@ -14,6 +14,7 @@ class Program
     // 1. 기존 객체의 속성 변경 - 안됨. 초기화되지 않은 변수도 
     //                                 받을수 있으므로 객체가 없을수 있다
     // 2. 새로운 객체 담아오기 - ok.!!
+
     public static void OutModifier(out Point pt)
     {
 //        pt.x = 2;   // error. 초기화 되지 않은 상태(객체가 없는)일수 있다
@@ -32,3 +33,8 @@ class Program
         WriteLine($"{p1.x} {p1.y}");
     }
 }
+
+// 일반적인 경우 
+// => reference type을 인자 전달시 ref, out 으로 사용하는 경우는 많지 않습니다.
+// => 단, value type 을 전달시는 ref, out 이 많이 사용될수 있습니다.
+//    Swap, TryParse 등... 
