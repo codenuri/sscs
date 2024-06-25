0a3298bfccb3d206721f293e2da7f2bbaab1c9df
ecourse <smkang@codenuri.co.kr>
2024-06-26 08:48:31 +0900

    Add files via upload

---
+++ DAY3/01_class3-1.cs
@@ -0,0 +1,43 @@
+﻿using static System.Console;
+
+// 용어 정리
+class Rect
+{
+    // class 의 상태를 나타내는 데이타
+    // C++      : 멤버 데이타
+    // C#, java : 필드(field)
+    public int left;
+    public int top;
+    public int right;
+    public int bottom;
+
+    // 동작을 나타내는 함수
+    // C++      : 멤버 함수
+    // C#, Java : 메소드(method)
+    public Rect(int x1, int y1, int x2, int y2)
+    {
+        left = x1;
+        top = y1;
+        right = x2;
+        bottom = y2;
+    }
+
+
+    public int GetArea()
+    {
+        return (left - right) * (bottom - top);
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Rect rc = new Rect(10, 10, 30, 30);
+
+        int n = rc.GetArea();
+
+    }
+}
+
+
