ec5f36fc8216826dd464a7991aac78df42993ef1
ecourse <smkang@codenuri.co.kr>
2024-06-26 14:09:18 +0900

    Add files via upload

---
+++ DAY3/07_static4.cs
@@ -0,0 +1,17 @@
+﻿
+class Program
+{
+    public static void Main()
+    {
+        // 이미 C#에 날짜 클래스 있습니다.
+        // => 정확히는 struct 입니다
+
+        DateTime dt = new DateTime(2025, 6, 26);
+
+        // 1000일뒤 날짜 구해 보세요
+        DateTime dest = 
+
+        Console.WriteLine(dest.ToString());
+
+    }
+}
