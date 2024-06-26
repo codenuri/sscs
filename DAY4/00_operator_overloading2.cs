f37e5b2d2ee11d8ce4479e2f1b32d56eea8d1b3b
ecourse <smkang@codenuri.co.kr>
2024-06-27 08:43:02 +0900

    Add files via upload

--- DAY4/00_operator_overloading2.cs
+++ DAY4/00_operator_overloading2.cs
@@ -6,6 +6,13 @@ class Point
     public int Y { get; set; } = 0;
 
     public Point(int a, int b) => (X, Y) = (a, b);
+
+    public static Point operator+( Point p1, Point p2 )
+    {
+        Point p3 = new Point(p1.X + p2.X, p1.Y + p2.Y);
+
+        return p3;
+    }
 }
 
 class Program
