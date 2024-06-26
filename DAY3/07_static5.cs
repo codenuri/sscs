74193349db609522c364af7a74b71a5f7ded8745
ecourse <smkang@codenuri.co.kr>
2024-06-26 14:23:20 +0900

    Add files via upload

--- DAY3/07_static5.cs
+++ DAY3/07_static5.cs
@@ -1,4 +1,7 @@
 ﻿// 아래 코드를 보고 에러를 모두 찾으세요
+// 핵심 
+// static method : 객체가 없어도 호출가능
+// static field  : 모든 객체가 공유, 객체가 없어도 메모리에 존재 
 /*
 class Example
 {
@@ -7,22 +10,27 @@ class Example
 
     public void M1()
     {
-        data1 = 0;  // 1
-        data2 = 0;  // 2
-        M1();       // 3
+        // M1 이 호출되었다는 것은 "객체가 있다" 는 의미. 
+        data1 = 0;  // 1    // ok 객체가 있으므로 data1도 있다
+        data2 = 0;  // 2    // ok
+        M2();       // 3    // ok
     }
-    static public void M2()
+    public static void M2()
     {
-        data1 = 0;  // 4
-        data2 = 0;  // 5
-        M1();       // 6
+        data1 = 0;  // 4. error
+        data2 = 0;  // 5. ok
+        M1();       // 6. error
+
+        M3(); // ok. 
     }
+    public static void M3() {}
 }
 */
 class Program
 {
     public static void Main()
     {
-
+        Example.M2();   // static 메소드 이므로 객체 없어도 언제라도
+                        // 호출가능
     }
 }
