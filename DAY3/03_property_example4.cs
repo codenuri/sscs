583e1e20d99b00194e3f9d205636a5f15e57bc49
ecourse <smkang@codenuri.co.kr>
2024-06-26 10:19:47 +0900

    Add files via upload

--- DAY3/03_property_example4.cs
+++ DAY3/03_property_example4.cs
@@ -1,9 +1,27 @@
-﻿class Person
+﻿using static System.Console;
+
+class Person
 {
     private string name;
     private int age;
 
     // Name, Age 속성 제공해 보세요
+    public string Name
+    { 
+        get => name;
+        set => name = value;
+    }
+    public int Age 
+    {
+        get => age;
+        set => age = value;
+    }
+
+    // Description 속성도 만드세요
+    public string Description
+    {
+        get => name + " " + age.ToString();
+    }
 
     public Person(string n, int a)
     {
@@ -16,7 +34,6 @@ class Program
     public static void Main()
     {
         Person p = new Person("kim", 20);
-
         WriteLine(p.Description); // "kim 20" 나오게해보세요
     }
 }
