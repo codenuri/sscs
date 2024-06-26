0167f864549521c30658fcf641734c534619d85e
ecourse <smkang@codenuri.co.kr>
2024-06-26 14:15:58 +0900

    Add files via upload

---
+++ DAY3/07_static5.cs
@@ -0,0 +1,28 @@
+﻿// 아래 코드를 보고 에러를 모두 찾으세요
+/*
+class Example
+{
+    public int data1 = 0;
+    public static int data2 = 0;
+
+    public void M1()
+    {
+        data1 = 0;  // 1
+        data2 = 0;  // 2
+        M1();       // 3
+    }
+    static public void M2()
+    {
+        data1 = 0;  // 4
+        data2 = 0;  // 5
+        M1();       // 6
+    }
+}
+*/
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
