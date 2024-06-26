c1606621ca86975196bdce596dfc318b47115e83
ecourse <smkang@codenuri.co.kr>
2024-06-26 14:38:48 +0900

    Add files via upload

--- DAY3/07_static6.cs
+++ DAY3/07_static6.cs
@@ -9,6 +9,11 @@ class Program
 {
     public static void Main()
     {
+        // #1. static method 는 객체없이 클래스 이름으로 호출
         Example.M1();
+
+        // #2. instance method 는 반드시 객체 필요
+        Example e = new Example();
+        e.M2(); // 반드시 객체 e가 있어야 합니다.
     }
 }
