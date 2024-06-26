74c4cd9ef0ef37b8fbe490ef483404081973601d
ecourse <smkang@codenuri.co.kr>
2024-06-26 09:41:23 +0900

    Add files via upload

--- DAY3/02_property2.cs
+++ DAY3/02_property2.cs
@@ -1,4 +1,11 @@
 // property(속성) : setter 와 getter 를 쉽게 만드는 문법
+// => 메소드 처럼 만들지만, 필드처럼 사용하게 됩니다.
+
+// 코딩 관례
+// 필드     : 모두 소문자로
+// Property : 1글자를 대문자로
+// Method   : 1글자를 대문자로
+// class이름 :1글자를 대문자로
 
 class Person
 {
