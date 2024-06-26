1bb2071384813a1132ea3f3225871e17fa6e5c95
ecourse <smkang@codenuri.co.kr>
2024-06-26 11:09:22 +0900

    Add files via upload

--- DAY3/04_auto_property2.cs
+++ DAY3/04_auto_property2.cs
@@ -9,15 +9,23 @@ class Example
 	
 
 	// 아래 코드에서 에러는 ?
-	public int Data3 { set; } = 0;	
-	public int Data4 { get; } = 0;	
+//	public int Data3 { set; } = 0;	// error. 필드 이름을 모르는데
+									// get 도 없다면
+									// 꺼낼 방법이 없습니다.
+									// auto 가 아니면 필드이름으로 꺼내면됩니다
+	
+	public int Data4 { get; } = 0;	// ok. set 이 없지만
+									// 생성자에서는 사용가능.
 
 
 	public Example(int a, int b)
 	{
-		Data2 = a;	
-		data2 = a;  
-		Data4 = a;  
+		// Data2 : get-only, 필드 이름 있음
+		// Data4 : get-only, 필드 이름 모름.
+//		Data2 = a;	// error.  필드이름 있으므로 아래 처럼 초기화 가능
+		data2 = a;  // OK.
+
+		Data4 = a;  // ok
 	}
 }
 
@@ -26,8 +34,8 @@ class Program
     public static void Main()
     {
         Example e1 = new Example(1, 1);
-	    e.Data2 = 10;	
-		e.Data4 = 10;	
+//	    e.Data2 = 10;	// error
+//		e.Data4 = 10;	// error
 
     }
 }
