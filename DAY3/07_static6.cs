2f19e795f05fd65b88e24a6a9036a0694509bbcc
ecourse <smkang@codenuri.co.kr>
2024-06-26 14:37:16 +0900

    Add files via upload

---
+++ DAY3/07_static6.cs
@@ -0,0 +1,14 @@
+﻿// 07_static6.cs
+class Example
+{
+    public static void M1() { }
+    public void M2() { }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Example.M1();
+    }
+}
