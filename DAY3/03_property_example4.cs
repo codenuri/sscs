15ec69a329b5cb31bed75adaac8ee3f52a60a035
ecourse <smkang@codenuri.co.kr>
2024-06-26 10:14:31 +0900

    Add files via upload

---
+++ DAY3/03_property_example4.cs
@@ -0,0 +1,22 @@
+﻿class Person
+{
+    private string name;
+    private int age;
+
+    // Name, Age 속성 제공해 보세요
+
+    public Person(string n, int a)
+    {
+        Name = n;
+        Age = a;
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        Person p = new Person("kim", 20);
+
+        WriteLine(p.Description); // "kim 20" 나오게해보세요
+    }
+}
