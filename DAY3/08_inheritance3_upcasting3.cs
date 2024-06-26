58dc12029d6f8ea8c1e7ed68266f728a723825f9
ecourse <smkang@codenuri.co.kr>
2024-06-26 17:08:35 +0900

    Add files via upload

---
+++ DAY3/08_inheritance3_upcasting3.cs
@@ -0,0 +1,27 @@
+﻿class Animal       { public int Age { get; set; } = 0; }
+
+class Dog : Animal { public int Color { get; set; } = 0;}
+class Cat : Animal { public int Speed { get; set; } = 0;}
+
+class Program
+{
+    // 모든 동물은 1년이 지나면 1살 증가합니다.
+    public static void NewYear(Dog obj)
+    {
+        ++(obj.Age);
+    }
+
+    public static void Main()
+    {
+        Dog d = new Dog();
+        Cat c = new Cat();
+
+        NewYear(d);
+        NewYear(c);
+
+        Dog[] arr = new Dog[10];
+
+    }
+
+
+}
