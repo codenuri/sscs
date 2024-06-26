fe6eb482f3068450ad4ff40f9a6f4ba57a5dfc8d
ecourse <smkang@codenuri.co.kr>
2024-06-27 08:50:05 +0900

    Add files via upload

---
+++ DAY4/00_operator_overloading3.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using System.Security.Cryptography;
+using static System.Console;
+
+class Point
+{
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+
+    public Point(int a, int b) => (X, Y) = (a, b);
+
+    public static Point operator +(Point p1, Point p2)
+    {
+        Point p3 = new Point(p1.X + p2.X, p1.Y + p2.Y);
+
+        return p3;
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Point p1 = new Point(1, 1);
+
+    }
+}
+
