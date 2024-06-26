e2f61fadfacc513f71ffbdb5e8822d68cfe0aff2
ecourse <smkang@codenuri.co.kr>
2024-06-26 15:54:59 +0900

    Add files via upload

---
+++ DAY3/08_inheritance2_constructor2-2.cs
@@ -0,0 +1,15 @@
+﻿class Person
+{
+    private string name;
+    private int age;
+
+    public Person() { }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Person p = new Person();
+    }
+}
