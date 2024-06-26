9e2dc4d52e67d824a6c16c7fcda6b9c68767613a
ecourse <smkang@codenuri.co.kr>
2024-06-26 15:31:33 +0900

    Add files via upload

---
+++ DAY3/08_inheritance1.cs
@@ -0,0 +1,41 @@
+﻿// #1. 상속을 사용하지 않는 경우
+// => name, age 가 중복된다.
+/*
+class Student
+{
+    private string name;
+    private int age;
+    private int id;
+}
+class Professor
+{
+    private string name;
+    private int age;
+    private int major;
+}
+*/
+// #2. 상속을 사용하는 코드
+
+// 모든 사람의 공통을 특징을 먼저 설계하고
+class Person
+{
+    private string name;
+    private int age;
+}
+class Student : Person
+{
+    private int id;
+}
+class Professor : Person
+{
+    private int major;
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Student s = new Student(); // name, age, id 를 가진 객체 생성
+    }
+}
+
