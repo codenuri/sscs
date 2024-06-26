f618c62395b3e5444ddd5d002e218a8b87379c4b
ecourse <smkang@codenuri.co.kr>
2024-06-26 15:35:50 +0900

    Add files via upload

--- DAY3/08_inheritance1.cs
+++ DAY3/08_inheritance1.cs
@@ -15,6 +15,7 @@ class Professor
 }
 */
 // #2. 상속을 사용하는 코드
+// 객체지향의 3대 특징 : "캡슐화", "상속", "다형성"
 
 // 모든 사람의 공통을 특징을 먼저 설계하고
 class Person
@@ -22,6 +23,8 @@ class Person
     private string name;
     private int age;
 }
+
+// 기존 클래스(Person) 을 확장(상속)해서 새로운 클래스(타입)설계하는 문법
 class Student : Person
 {
     private int id;
@@ -39,3 +42,8 @@ class Program
     }
 }
 
+// 거의 모든 객체지향 언어는 상속을 지원합니다. 표기법만 다릅니다.
+// C#     : class Student : Person {}
+// C++    : class Student : public Person {}
+// Java   : class Student extends  Person {}
+// Python : class Student(Person) {}
