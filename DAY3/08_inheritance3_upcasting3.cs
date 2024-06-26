b114ce9e3b1c60b2e4d1236743a317d70879a7cb
ecourse <smkang@codenuri.co.kr>
2024-06-26 17:15:24 +0900

    Add files via upload

--- DAY3/08_inheritance3_upcasting3.cs
+++ DAY3/08_inheritance3_upcasting3.cs
@@ -5,11 +5,21 @@ class Cat : Animal { public int Speed { get; set; } = 0;}
 
 class Program
 {
+    // Upcasting 의 활용(장점) #1
+    // => 동종을 처리하는 메소드를 만들수 있다.
+    // => 모든 동물을 인자로 받을수 있는 메소드
+
     // 모든 동물은 1년이 지나면 1살 증가합니다.
-    public static void NewYear(Dog obj)
+//  public static void NewYear(Dog obj) // Dog 만 전달 받을수 있다.
+    public static void NewYear(Animal obj)  // 모든 동물을 받을수 있다
     {
         ++(obj.Age);
     }
+
+    // 참고
+//  public static void NewYear(object obj) { }
+
+
 
     public static void Main()
     {
@@ -19,7 +29,12 @@ class Program
         NewYear(d);
         NewYear(c);
 
-        Dog[] arr = new Dog[10];
+        // Upcasting 활용(장점) #2. 동종을 보관하는 배열(컬렉션)만들기
+        Dog[] arr1 = new Dog[10];        // Dog의 객체의 참조만 보관 가능
+        Animal[] arr2 = new Animal[10]; // 모든 동물의 참조를 보관가능
+
+        arr2[0] = new Dog();
+        arr2[1] = new Cat();
 
     }
 
