a0c1f4dcfe9af38ea4d7adcc65f0e63a97232570
ecourse <smkang@codenuri.co.kr>
2024-06-27 13:00:03 +0900

    Add files via upload

--- DAY4/object2.cs
+++ DAY4/object2.cs
@@ -9,7 +9,16 @@
         // obj 가 가리키는 객체의 타입을 알고 싶다.!!
         // 방법 #1. is 연산자
 
+        bool b = obj is string; // if ( obj is string )
+
 
         // 방법 #2. reflection 
+        // => object 클래스가 제공하는 7개 메소드 중에 "GetType()" 사용
+
+        Type t = obj.GetType(); // obj 가 가리키는 타입에 대한 정보를 담은
+                                // Type 객체 반환
+
+        Console.WriteLine( t.Name);
+        Console.WriteLine( t.BaseType?.Name);
     }
 }
