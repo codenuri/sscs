829aef9e9b90e5ff7005e1a53dfba66ce3291299
ecourse <smkang@codenuri.co.kr>
2024-06-27 11:13:46 +0900

    Add files via upload

--- DAY4/01_override5.cs
+++ DAY4/01_override5.cs
@@ -2,6 +2,9 @@ using static System.Console;
 
 // 1. non-virtual 은 파생 클래스에서 되도록 다시 만들지 마세요
 
+// 2. 파생 클래스가 다신 만들수 있게 하려면 virtual 로 하세요
+//    파생 클래스가 override 할때는 "new" 도 가능하지만 되도록 하지 마세요!!
+//												(나쁜 디자인)
 class Base
 {
 	public void M1() {}
@@ -17,7 +20,8 @@ class Derived : Base
 
     // virtual 함수를 다시 만들때 "override" "new" 모두 가능합니다.
     public override void M3() { WriteLine("Derived M3"); }
-    public new void M4()      { WriteLine("Derived M4"); }
+    public new void M4()      { WriteLine("Derived M4"); } 
+								// 사용하지 마세요. 혼란의 원인
 }
 
 class Program 
@@ -27,7 +31,7 @@ class Program
 		Base b = new Derived();
 		b.M1(); //
 
-		b.M3(); 
-		b.M4();
+		b.M3(); // Derived M3
+		b.M4(); // Base M4
 	}
 }
