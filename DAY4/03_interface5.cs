82c5a3d01476dd23f8b410a9ea7abaee64c06d3c
ecourse <smkang@codenuri.co.kr>
2024-06-27 16:01:07 +0900

    Add files via upload

--- DAY4/03_interface5.cs
+++ DAY4/03_interface5.cs
@@ -1,21 +1,34 @@
 ﻿using static System.Console;
 
-
-class Shape
+// 추상 메소드가 한개 이상있는 클래스는 반드시 
+// "abstract class" 라고 해야 합니다.
+abstract class Shape
 {
     private int color;
 
     public void SetColor(int c) { color = c; }
     public int GetColor() { return color; }
 
-    public virtual int GetArea() { return 0; }
+
 
     // 가상 메소드 : 재정의 하지 않으면 기본 구현 제공해 줄께!!
     //              재정의 해서 바꿀수도 있어
 
     // 추상 메소드 : 반드시 재정의해야 한다고 지시 하는 것
 
-    public virtual void Draw() { WriteLine("Draw Shape"); }
+    //    public virtual void Draw() { WriteLine("Draw Shape"); }
+    public abstract void Draw();
+
+
+    // GetArea 는 어떻게 할까 ?
+    // 구현을 제공하면 : 파생 클래스는 만들지 않아도 기본 구현 제공 
+    // 추상메소드 라면 : 파생 클래스는 만드시 만들어야 한다
+    // 또 다른 방법 : 예외 발생
+
+    // 1. Rect에서 override 하지 않았는데, 호출하면 예외(runtime 오류)
+    // 2. Rect에서 override 하지 않았는데, 사용하지 않으면 ok.
+    // 3. Rect에서 override 하고 사용하면 ok. 
+    public virtual int GetArea() { throw new NotImplementedException(); }
 }
 
 
