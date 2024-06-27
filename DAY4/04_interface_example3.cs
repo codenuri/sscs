9e27c6c4a77880ca036173e41291eac154fc7b0b
ecourse <smkang@codenuri.co.kr>
2024-06-27 16:47:41 +0900

    Add files via upload

--- DAY4/04_interface_example3.cs
+++ DAY4/04_interface_example3.cs
@@ -1,5 +1,26 @@
 using static System.Console;
 
+/*
+// 객체가 자신과 동일하게 생긴 복사본을 만드는 작업은
+// 자주 사용되는 작업입니다.
+// 메소드 이름을 인터페이스로 약속합니다.
+interface ICloneable
+{
+    Object Clone();
+}
+
+// 배열은 복제개념을 제공합니다.
+class Int32[] : ICloneable 
+{
+    public Object Clone()
+    {
+        // 자신과 동일한 요소를 가지는 배열을 한개 더생성해서
+        //  반환합니다.
+    }
+}
+*/
+
+
 class Program
 {
     public static void Main()
@@ -15,12 +36,13 @@ class Program
         int[] arr1 = { 1, 2, 3 };
 
         int[] arr2 = arr1; // arr2 는 arr1 과 같은 배열 공유
-        
-        int[] arr3 = arr1.Clone(); // arr1 과 동일한 배열을 한개 더생성해서
-                                   // arr3가 가리키겠다는 의도
+
+//      int[] arr3 = arr1.Clone(); // arr1 과 동일한 배열을 한개 더생성해서
+                                            // arr3가 가리키겠다는 의도
 
         // 위 코드는 왜 에러 일까요 ?
         // 해결책은 ?
+        int[] arr3 = (int[])arr1.Clone();
     }
 }
 
