e8766fe49aadaa45469aec91bbf765a244149136
ecourse <smkang@codenuri.co.kr>
2024-06-27 12:41:43 +0900

    Add files via upload

---
+++ DAY4/object1.cs
@@ -0,0 +1,15 @@
+﻿// object1.cs
+
+using System.Drawing;
+
+class Program
+{
+    public static void Main()
+    {
+        // 이미 C# 언어에 "Rectangle", "Point" 가 있습니다.
+        Rectangle r = new Rectangle(1, 1, 1, 1);
+
+        Point p = new Point(1, 1);
+
+    }
+}
