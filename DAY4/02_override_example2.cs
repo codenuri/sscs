06bc13659d4d59def3fe1b013f4a935064276831
ecourse <smkang@codenuri.co.kr>
2024-06-27 10:04:53 +0900

    Add files via upload

---
+++ DAY4/02_override_example2.cs
@@ -0,0 +1,23 @@
+using static System.Console;
+
+// 파워 포인트 같은 프로그램을 객체지향 언어로 만드는 것을 생각해 봅시다.
+
+// 1. 모든 도형을 타입으로 설계하면 편리합니다
+
+class Rect
+{
+	// int x, y, w, h, 생성자등은 생략, 있다고 가정하고 가상함수에 예제에집중
+
+	public void Draw() { WriteLine("Draw Rect"); }
+}
+
+
+
+class Program 
+{
+	public static void Main()
+	{
+
+
+	}
+}
