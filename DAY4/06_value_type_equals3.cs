a893bdf87b88807d39b746a96b01bf8cc29d946d
ecourse <smkang@codenuri.co.kr>
2024-06-27 14:50:45 +0900

    Add files via upload

---
+++ DAY4/06_value_type_equals3.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+using static System.Console;
+
+struct SPoint
+{
+    public int X { set; get; } = 0;
+    public int Y { set; get; } = 0;
+    public SPoint(int a, int b) => (X, Y) = (a, b);
+}
+class Program
+{
+    public static void Main()
+    {
+        SPoint sp1 = new SPoint(1, 2);
+        SPoint sp2 = sp1;
+
+
+        WriteLine($"{object.ReferenceEquals(sp1, sp2)}");
+
+    }
+
+}
