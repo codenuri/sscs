0ac712c20456c76f324083595a2464aaebf1fc2b
ecourse <smkang@codenuri.co.kr>
2024-06-27 13:06:43 +0900

    Add files via upload

--- DAY4/object2.cs
+++ DAY4/object2.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Reflection;
+
+class Program
 {
     public static void Main()
     {
@@ -15,10 +17,26 @@
         // 방법 #2. reflection 
         // => object 클래스가 제공하는 7개 메소드 중에 "GetType()" 사용
 
-        Type t = obj.GetType(); // obj 가 가리키는 타입에 대한 정보를 담은
+        Type t = obj.GetType(); // obj 가 "가리키는 타입"에 대한 정보를 담은
                                 // Type 객체 반환
 
-        Console.WriteLine( t.Name);
-        Console.WriteLine( t.BaseType?.Name);
+        //      Console.WriteLine( t.Name);
+        //      Console.WriteLine( t.BaseType?.Name);
+
+        Console.WriteLine( t.FullName);
+        Console.WriteLine( t.BaseType?.FullName);
+
+        // C# 의 reflection(타입정보를 얻는기술)은 아주 뛰어납니다.
+        var methods = t.GetMethods(); // 모든 메소드의 정보를 배열로 얻기
+        // MethodInfo[] methods 
+
+        foreach (MethodInfo mi in methods)
+        {
+            Console.WriteLine($"{mi.Name}, {mi.ReturnType}");
+
+            // 파라미터도 알고 싶으면
+            //var parameters = mi.GetParameters();
+        }
+
     }
 }
