a070927b65b7e9bd1c37746740f14616e854ba0e
ecourse <smkang@codenuri.co.kr>
2024-06-27 11:15:41 +0900

    Add files via upload

---
+++ DAY4/02_override_example7.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+
+class Point
+{
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+
+    public Point(int a, int b) => (X, Y) = (a, b);
+}
+
+class Program
+{
+    public static void Main()
+    {
+
+    }
+}
+
+
+
