3ab6187174cb4341890e08a87a3aca5b3e564d03
ecourse <smkang@codenuri.co.kr>
2024-06-27 13:26:25 +0900

    Add files via upload

--- DAY4/05_equals2.cs
+++ DAY4/05_equals2.cs
@@ -1,9 +1,28 @@
 // 107 page ~  
+using System.Runtime.Remoting;
+
 class Point
 {
     public int X { set; get; } = 0;
     public int Y { set; get; } = 0;
     public Point(int a, int b) => (X, Y) = (a, b);
+
+
+    // object 가 제공하는 Equals 의 기본 구현은 객체의 동질성(==) 입니다.
+    // 되도록 사용자가 override 해서 "상태의 동질성 코드"로 변경하는 것을 권장
+
+    // 객체의 동질성을 알고 싶으면 : == 사용
+    // 상태의 동질성을 알고 싶으면 : Equals() 사용하도록!!
+
+    public override bool Equals(object? obj)
+    {
+        Point? other = obj as Point;
+
+        // 혹시 null 이면 같지 않다로 처리!!
+        if (other == null) return false;
+
+        return X == other.X && Y == other.Y;    
+    }
 }
 
 class Program
@@ -22,6 +41,9 @@ class Program
 
 
         // 방법 #2. object 로 부터 상속 받는 Equals()가상함수 사용
+        // => object 안에 있는 기본 구현은  "==" 로 비교
+        //    즉, 기본 구현은 "객체의 동일성" 조사
+
         Console.WriteLine($"{p1.Equals(p2)}, {p3.Equals(p4)}");
     }
 }
