064c1dee8cc7d52073608bc06ad8b10c960a8c57
ecourse <smkang@codenuri.co.kr>
2024-06-27 13:20:46 +0900

    Add files via upload

--- DAY4/05_equals2.cs
+++ DAY4/05_equals2.cs
@@ -15,8 +15,13 @@ class Program
         Point p3 = new Point(1, 2);
         Point p4 = new Point(1, 2);
 
+        // Reference Type 인 경우. 
         // 방법 #1. == 연산자 사용
-        Console.WriteLine($"{p1 == p2}, {p3 == p4}");
+        // => 객체의 동일성 조사 ( 동일 객체인가)
+        Console.WriteLine($"{p1 == p2}, {p3 == p4}"); // True, False
 
+
+        // 방법 #2. object 로 부터 상속 받는 Equals()가상함수 사용
+        Console.WriteLine($"{p1.Equals(p2)}, {p3.Equals(p4)}");
     }
 }
