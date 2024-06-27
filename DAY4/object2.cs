94cf755048fc84338d28b94ad6b7cdc19a4da4f3
ecourse <smkang@codenuri.co.kr>
2024-06-27 12:56:14 +0900

    Add files via upload

---
+++ DAY4/object2.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        string s = "hello";
+
+        object obj = s;
+
+        // obj 가 가리키는 객체의 타입을 알고 싶다.!!
+        // 방법 #1. is 연산자
+
+
+        // 방법 #2. reflection 
+    }
+}
