3d9b146922b0d1096c231137368dfbdd4fcf4678
ecourse <smkang@codenuri.co.kr>
2024-06-27 14:48:03 +0900

    Add files via upload

--- DAY4/06_value_type_equals2.cs
+++ DAY4/06_value_type_equals2.cs
@@ -15,6 +15,14 @@ class Program
         SPoint sp2 = new SPoint(1, 2);
 
         // #2. Equals() 가상 메소드
+        // => Object 의 기본 구현은 " == " 사용. 
+        // => ValueType 클래스에서 "Equals" 를 override 해서
+        //    2개 객체의 메모리를 통째로 비교(C언어의 memcmp 같이)
+        //    상태 동질성 비교
+
+        // => 일부 멤버만 비교하도록 하려면 override 하면 됩니다.
         WriteLine($"{sp1.Equals(sp2)}");
+
+    }
 
 }
