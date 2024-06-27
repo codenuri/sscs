7f8e1186a63ca48ebf9a755530ec5641c8db1dcf
ecourse <smkang@codenuri.co.kr>
2024-06-27 15:51:41 +0900

    Add files via upload

---
+++ DAY4/03_interface5.cs
@@ -0,0 +1,55 @@
+﻿using static System.Console;
+
+
+class Shape
+{
+    private int color;
+
+    public void SetColor(int c) { color = c; }
+    public int GetColor() { return color; }
+
+    public virtual int GetArea() { return 0; }
+
+
+    public virtual void Draw() { WriteLine("Draw Shape"); }
+}
+
+
+
+class Rect : Shape
+{
+    public override void Draw() { WriteLine("Draw Rect"); }
+}
+class Circle : Shape
+{
+    public override void Draw() { WriteLine("Draw Circle"); }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        List<Shape> list = new List<Shape>();
+
+        while (true)
+        {
+            int cmd = int.Parse(Console.ReadLine() ?? "0");
+
+            if (cmd == 1) list.Add(new Rect());
+            else if (cmd == 2) list.Add(new Circle());
+            else if (cmd == 9)
+            {
+                foreach (var s in list)
+                {
+
+                    s.Draw();
+
+
+                }
+            }
+        }
+    }
+}
+
+
+
