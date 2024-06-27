5ceef875f917ed417e475b96586522419a5483a1
ecourse <smkang@codenuri.co.kr>
2024-06-27 15:45:28 +0900

    Add files via upload

---
+++ DAY4/03_interface4.cs
@@ -0,0 +1,25 @@
+﻿using static System.Console;
+
+
+interface ICamera
+{
+//  private int color;  // error. 필드를 가질수 없습니다.
+
+//  public void SetColor(int c) => color = c; // error. 메소드 제공안됨
+
+    void Take();  // ok
+
+    int Weight { get; }; // ?
+}
+
+
+
+class Program
+{
+    public static void Main()
+    {
+ 
+
+    }
+}
+
