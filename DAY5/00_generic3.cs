aba8bc904f90abe2d3915c4e37ee7148b44a3135
ecourse <smkang@codenuri.co.kr>
2024-06-28 08:49:55 +0900

    Add files via upload

--- DAY5/00_generic3.cs
+++ DAY5/00_generic3.cs
@@ -20,6 +20,17 @@ class Program
         // generic 메소드는 함수 인자로 타입 추론이 가능합니다.
         // "swap<int>" 같이 타입을 전달해도 되고, 생략해도 됩니다
         swap(ref n1, ref n2);
-    
+
+
+        M1(1, 1.2);
+        M2(1, 1.2);
+        M3(1, 1.2);
+        M4(1);
+
     }
+
+    public static void M1<T>(T a, T b) { }
+    public static void M2<T1, T2>(T1 a, T2 b) { }
+    public static void M3<A, B>(A a, B b) { } // 아무문자나 사용가능
+    public static R M4<R, T>(T a) { } // 반환 타입
 }
