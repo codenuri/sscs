41975fef1bd48d5551c1422d716f07ddd028670a
ecourse <smkang@codenuri.co.kr>
2024-06-27 16:02:32 +0900

    Add files via upload

---
+++ DAY4/04_interface_example1.cs
@@ -0,0 +1,29 @@
+
+using static System.Console;
+
+class Program 
+{
+	public static void Main()
+	{
+		int n1 = 10;
+		int n2 = 20;
+
+		string s1 = "AAA";
+		string s2 = "BBB";
+
+		// 객체의 크기 비교 방법
+
+		// 방법 #1. 비교 연산자 (<, > 사용)
+		bool b1 = n1 < n2;
+        bool b2 = s1 < s2;
+
+    }
+
+	
+
+
+	public static void Foo(int arg)
+	{
+	}
+
+}
