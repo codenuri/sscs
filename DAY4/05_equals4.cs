a4583e40df09b5b2bf61d2e0ffe599c0542d629e
ecourse <smkang@codenuri.co.kr>
2024-06-27 13:55:09 +0900

    Add files via upload

--- DAY4/05_equals4.cs
+++ DAY4/05_equals4.cs
@@ -9,15 +9,14 @@ class Point
 
     // == 는 기본적으로를 "객체의 동질성(동일객체인가)" 입니다
     // 그런데, 연산자 재정의를 통해서 "상태의 동질성"으로 변경할수 있습니다.
-
-    static bool operator==(Point a, Point b)
+    public static bool operator==(Point a, Point b)
     {
         return a.X == b.X && a.Y == b.Y;
     }
     // == 제공시 != 도 같이 제공하는 것이 원칙 입니다.
-    static bool operator !=(Point a, Point b)
+    public static bool operator !=(Point a, Point b)
     {
-        return !(a== b);
+        return !(a == b);
     }
 }
 
@@ -28,7 +27,7 @@ class Program
         Point p1 = new Point(1, 2);
         Point p2 = new Point(1, 2);
 
-        Console.WriteLine($"{p1 == p2}");
+        Console.WriteLine($"{p1 == p2}");  // true
 
     }
 }
