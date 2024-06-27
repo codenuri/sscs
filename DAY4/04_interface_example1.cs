4e8fdd0421533c8f95e4379ba84c1d616e75259b
ecourse <smkang@codenuri.co.kr>
2024-06-27 16:07:20 +0900

    Add files via upload

--- DAY4/04_interface_example1.cs
+++ DAY4/04_interface_example1.cs
@@ -14,9 +14,18 @@ class Program
 		// 객체의 크기 비교 방법
 
 		// 방법 #1. 비교 연산자 (<, > 사용)
-		bool b1 = n1 < n2;
-        bool b2 = s1 < s2;
+		bool b1 = n1 < n2; // ok. 수치 타입은 가능
+//      bool b2 = s1 < s2; // error. string 은 지원안함
 
+		// 방법 #2. CompareTo 메소드
+
+		int ret1 = n1.CompareTo(n2); // n1 이 크면 양수, 작으면 음수, 같으면 0
+        int ret2 = s1.CompareTo(s2); // string 도 지원.
+
+		// 핵심 : C#이 제공하는 타입에서  크기 비교가 가능한 모든 타입은
+		//        "CompareTo" 메소드 있습니다.
+
+		IComparable c = s1;
     }
 
 	
