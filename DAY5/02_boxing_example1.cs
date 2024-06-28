ac59f00fd4d6623e8d8afeb7c986cbeccc67e69f
ecourse <smkang@codenuri.co.kr>
2024-06-28 09:34:17 +0900

    Add files via upload

---
+++ DAY5/02_boxing_example1.cs
@@ -0,0 +1,28 @@
+using static System.Console;
+
+class Digit : IComparable
+{
+    public int Value { get; set; }
+
+    public Digit(int n) => Value = n;
+
+    // #1. 인자로 정확한 타입을 표시했습니다
+    public int CompareTo(Digit other)
+    {
+        return Value.CompareTo(other.Value);    
+    }
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Digit d1 = new Digit(3);
+        Digit d2 = new Digit(4);
+
+        int ret = d1.CompareTo(d2);
+
+    }
+}
+
+
