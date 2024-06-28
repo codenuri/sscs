11ff98e7150c47f40b69866636f367b122ea43e4
ecourse <smkang@codenuri.co.kr>
2024-06-28 12:47:38 +0900

    Add files via upload

--- DAY5/05_example3.cs
+++ DAY5/05_example3.cs
@@ -19,8 +19,12 @@ class Program
         int ret2 = MyFindIndex(x2, Foo );
 
         WriteLine($"{ret2}"); // 2
+
+        // Array.FindIndex() 구현 확인해 보세요
+        Array.FindIndex(x2, Foo);
     }
 
+    // 아래 코드가 실제 Array.FindInex 와 거의 동일합니다.
     public static int MyFindIndex<T>(T[] arr, Predicate<T> match)
     {
         int sz = arr.Length;
