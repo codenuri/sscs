b2777337d60425aab7c6da64e6cbd384375df193
ecourse <smkang@codenuri.co.kr>
2024-06-28 14:13:31 +0900

    Add files via upload

---
+++ DAY5/08_lambda1.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+class Program
+{
+    static bool IsEven(int n) { return n % 2 == 0; }
+
+	public static void Main()
+    {
+        int[] array = { 1, 3, 6, 4, 5 };
+
+		int idx1 = Array.FindIndex(array, IsEven);
+
+        int idx2 = Array.FindIndex(array, bool (int n) => { return n % 2 == 0; } );
+		
+    }
+}
