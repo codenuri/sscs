42b9046aabbee0eee6e74b587d2b65533992a143
ecourse <smkang@codenuri.co.kr>
2024-06-28 10:20:40 +0900

    Add files via upload

--- DAY5/03_delegate3.cs
+++ DAY5/03_delegate3.cs
@@ -11,10 +11,22 @@ class Program
     public static void Main()
     {
         // #1. 메소드 여러개 등록 가능합니다.
+        // 추가 : +=
+        // 제거 : -=
         MyFunc f1 = Foo;
         f1 += Goo;
 
         f1(10); // Foo(10), Goo(10)
+
+
+        // #2. delegate 는 "reference type", "immutable" 합니다.
+
+        MyFunc f2 = Foo;
+        MyFunc f3 = f2;
+
+        f2 += Goo;
+
+        f3(10); // 결과 예측해 보세요.
   
     }
 
