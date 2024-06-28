1d459e522e8738ea7311c742836ee98e5127e8fb
ecourse <smkang@codenuri.co.kr>
2024-06-28 13:18:33 +0900

    Add files via upload

--- DAY5/06_event3.cs
+++ DAY5/06_event3.cs
@@ -1,10 +1,19 @@
 using static System.Console;
 
-delegate void Handler(Button source);
+
 
 class Button
 {
-    public Handler Click = null; // 버튼을 누를때 호출될 함수등록
+    // delegate 를 class 안에 만들어도 됩니다.
+    // Button 안에서만 사용하겠다는 의도
+    public delegate void Handler(Button source);
+
+    //  public Handler Click = null; // 버튼을 누를때 호출될 함수등록
+
+    // 위와 같이 Handler delegate 를 직접 사용하지 말고
+    // 앞에 event 를 붙이면 = 은 안되고 +=만 됩니다.
+    // => 안전하게 사용하기 위해서!
+    public event Handler? Click = null;
 
     public void UserPressButton()
     {
@@ -24,7 +33,8 @@ class Program
 
         btn1.Click = Foo;
         btn1.Click += Goo;
-        btn1.Click += Hoo;
+//      btn1.Click += Hoo;
+        btn1.Click = Hoo; // += 한다는 것을 =로 실수했다
 
         btn1.UserPressButton(); 
     }
