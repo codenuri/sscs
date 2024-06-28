1cb04f258706aea10933b3e0f0c5481019d536d8
ecourse <smkang@codenuri.co.kr>
2024-06-28 15:09:19 +0900

    Add files via upload

---
+++ DAY5/10_exception1.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+// #1. 대부분 메소드는 주어진 작업을 수행하다가 실패 할수도 있다
+
+// 방법 #1. 반환값으로 실패 전달
+// => 호출자가 "함수의 실패" 를 무시할수도 있습니다.
+// => 함수의 심각한 오류 발생을 무시하는 경우, 더욱 큰 문제가 될수있습니다.
+
+class DB
+{
+    public bool Backup() 
+    {
+        return false;
+    }
+
+    public void Remove() { WriteLine("Remove DB");  }
+}
+class Program
+{
+    public static void Main()
+    {
+        DB db = new DB();
+
+        bool ret = db.Backup();
+
+        // if ( ret == false)  {  }
+
+        db.Remove();
+    }
+}
+
+
---
+++ DAY5/10_exception2.cs
@@ -0,0 +1,40 @@
+﻿using static System.Console;
+
+// 객체지향 언어의 오류 처리 방법
+// #1. 메소드가 실패하면 "예외"를 던집니다.
+
+
+// 실패의 다양한 원인을 담은 클래스를 설계 합니다.
+class DBBackupFailException
+{
+    // 이안에 왜 실패 했는지의 정확한 정보를 전달하기 위한
+    // 다양한 멤버를 추가하세요
+}
+
+class DB
+{
+    public void Backup()
+    {
+        if ( 1 ) // 실패라고 가정
+        { 
+            throw new DBBackupFailException(); 
+        }
+    }
+
+    public void Remove() { WriteLine("Remove DB"); }
+}
+class Program
+{
+    public static void Main()
+    {
+        DB db = new DB();
+
+        bool ret = db.Backup();
+
+        // if ( ret == false)  {  }
+
+        db.Remove();
+    }
+}
+
+
