7f93a124357f5ebb1d54afa48c74b830ead30aa7
ecourse <smkang@codenuri.co.kr>
2024-06-28 11:10:14 +0900

    Add files via upload

---
+++ DAY5/05_example1.cs
@@ -0,0 +1,22 @@
+using static System.Console;
+
+class Program 
+{
+	public static void Main()
+	{
+		int[] x = { 1, 8, 6, 4, 3 };
+
+		// Array : 배열관련 다양한 static method 를 제공하는 클래스
+		// => 배열 관련 어떤 기능이 필요하면 "Array"에서 찾으세요
+
+		// #1. 배열에서 "3"이 있는 위치를 찾고 싶다. 값 검색
+		int ret1 = Array.IndexOf(x, 3);
+		WriteLine($"{ret1}"); // 4
+
+
+        // #2. 배열에서 처음 나오는 "짝수"를 찾고 싶다. 조건검색
+        int ret2 = Array.FindIndex(x, 메소드);
+        WriteLine($"{ret2}"); 
+    }
+
+}
