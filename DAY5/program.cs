320e7ae9d4517ed01986e47baa13e5a943935396
ecourse <smkang@codenuri.co.kr>
2024-06-28 15:59:23 +0900

    Add files via upload

---
+++ DAY5/program.cs
@@ -0,0 +1,20 @@
+﻿// CMC.cs  와 program.cs 만 빌드 풀러놓으세요
+
+class Program
+{
+    public static void Main()
+    {
+        string s = "aa";
+        int n = 10;
+
+        Console.WriteLine($"{CMC.SizeOf<int>()}");
+
+        CMC.PrintHeapObjectAddr(s);
+
+        unsafe
+        {
+            CMC.PrintAddr(&n);
+        }
+
+    }
+}
