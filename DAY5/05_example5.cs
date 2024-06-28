7b733e05a02f2ebb02173399b9f9435d56b7ee4a
ecourse <smkang@codenuri.co.kr>
2024-06-28 13:00:54 +0900

    Add files via upload

---
+++ DAY5/05_example5.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+class Program
+{
+    public static int MyComparer(int a, int b)
+    {
+        //		return a.CompareTo(b); // 오름 차순
+        return b.CompareTo(a); // 내림 차순
+    }
+
+    public static void Main()
+    {
+        int[] x = { 1, 3, 5, 7, 9, 2, 4, 6, 8, 10 };
+
+        // Sort 등을 사용할때 비교 정책을 변경하는 방법
+
+        // #1. Sort 메소드 인자로 "약속된 모양의 메소드 전달" - delegate 방식   
+        Array.Sort(x, MyComparer);
+
+
+        // #2. Sort 메소드 인자로 "약속된 함수를 구현한 객체 전달"
+        MyComp mc = new MyComp();
+        Array.Sort(x, mc);
+
+
+
+        foreach (var e in x)
+            Write($"{e}, ");
+    }
+
+}
+
