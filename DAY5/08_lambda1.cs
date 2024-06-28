ba6a01dee7752a3f656b96919d16f8ed98457917
ecourse <smkang@codenuri.co.kr>
2024-06-28 14:23:32 +0900

    Add files via upload

--- DAY5/08_lambda1.cs
+++ DAY5/08_lambda1.cs
@@ -10,7 +10,24 @@ class Program
 
 		int idx1 = Array.FindIndex(array, IsEven);
 
+        // lambda expression : 익명의 메소드를 만드는 문법
+        
+        // #1. 정확한 표기법
         int idx2 = Array.FindIndex(array, bool (int n) => { return n % 2 == 0; } );
-		
+
+        // #2. C# 의 특징은 "다양한 편의 표기법이 있다는 점."
+        // 람다 표현식도 역시 편의 표기법 제공
+
+        Array.FindIndex(array, (int n) => { return n % 2 == 0; }); // return type 추론가능
+
+        Array.FindIndex(array, (int n) => n % 2 == 0 ); // expression bodied 가능.
+
+        Array.FindIndex(array, (n) => n % 2 == 0); // 인자의 타입도 생략 가능.
+                                                   // array 가 int 라는 것을 알기 때문에
+
+        Array.FindIndex(array, n => n % 2 == 0); // 인자가 한개이면 () 없어도 됨
+
+ //       Array.FindIndex(array, $1 % 2 == 0); // swift 는 이렇게도 됩니다
+
     }
 }
