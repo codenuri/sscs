c65bd64d9a79005e7fd892216d22612fef39d89f
ecourse <smkang@codenuri.co.kr>
2024-06-28 11:00:27 +0900

    Add files via upload

--- DAY5/04_delegate_method2.cs
+++ DAY5/04_delegate_method2.cs
@@ -14,12 +14,17 @@ class Program
 		// #2. 이번에는 자신의 static, instance method 이야기
 
 		// 먼저 직접 호출
-		Program.SMethod(1);
+		Program.SMethod(1); // 정확한 표기법. "클래스이름.static 메소드이름"
+		SMethod(2);         // 자신의 메소드에서는 "클래스이름 생략가능"
+
+//		IMethod(1); // error. instance method 는 객체가 필요
+
+		Program p = new Program();
+		p.IMethod(1); // ok
 
 
-
-
-
+		// 위 코드를 잘생각해 보고 아래 ? 에 ok. error 로 채우세요
+		// error 인 경우, 수정해 보세요
 		MyFunc f1 = Program.SMethod; // ?
 		MyFunc f2 = SMethod;		 // ?
 		MyFunc f3 = IMethod;		 // ?
